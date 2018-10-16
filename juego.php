14847e0d871d9fee0d3a33d7ba18651b3b558839
Administrator <Administrator@192.168.145.131>
2018-10-16 20:54:14 +0200

    sistema de sesiones de php añadido

--- juego.php
+++ juego.php
@@ -36,36 +36,88 @@ $listaPreguntas = array();
     <p><a  class="btn btn-block btn-warning" onclick="volver();">Volver al Menú</a></p>
     
     <p><a id="sigue1" class="btn btn-block btn-primary" ><?php echo $tema;?></a></p>
-     
+    
+        <div id="cajatiempo" style="height: 30px;" >
+            <div id="tiempo" class="progress-bar progress-bar-striped bg-success" style="width: 0%;"></div>
+        </div>
+    <p></p>
     <p><a id="enunciado" class="btn btn-block btn-primary " ></a></p>
     
-    <p><a id="r1" class="btn btn-block btn-success " ></a></p>
-    <p><a id="r2"  class="btn btn-block btn-success " ></a></p>
-    <p><a id="r3"  class="btn btn-block btn-success " ></a></p>
-    <p><a id="r4"  class="btn btn-block btn-success " ></a></p>
+    <p><a id="r1"  class="btn btn-block btn-success " onclick="comprobadora(1);" ></a></p>
+    <p><a id="r2"  class="btn btn-block btn-success " onclick="comprobadora(2);" ></a></p>
+    <p><a id="r3"  class="btn btn-block btn-success " onclick="comprobadora(3);" ></a></p>
+    <p><a id="r4"  class="btn btn-block btn-success " onclick="comprobadora(4);" ></a></p>
+    <p><a id="correcta"  class="btn btn-block btn-success "></a></p>
 </div>
 
 <script>
     function volver(){
         $('#principal').load('app.php');
     }
+    var progreso;
+    var segundo = 0;
+    //temporizador de la barra
+    clearInterval(progreso);
+    progreso = setInterval(function(){
+        var caja = $("#cajatiempo");
+        var tiempo = $("#tiempo");
+        
+        if (tiempo.width() >= caja.width()){
+            tiempo.width(tiempo.width() - caja.width());
+            sigue();
+            segundo = "";
+            
+            
+        }
+        else{
+            tiempo.width(tiempo.width() + caja.width()/10);
+            segundo++;
+        }
+        //cambia el color de la barra dependiendo del segundo en que está
+        if (segundo < 5){
+            tiempo.removeClass("bg-warning").removeClass("bg-danger").addClass("bg-success");
+        }
+        else if (segundo < 8){
+            tiempo.removeClass("bg-success").addClass("bg-warning");
+        }
+        else {
+            tiempo.removeClass("bg-warning").addClass("bg-danger");
+        }
+        tiempo.text(segundo);
+        
+        
+    }, 1000);
     
+   
     //cargo el array php de preguntas en una variable javascript
     var listaPreguntas = <?php echo json_encode($listaPreguntas); ?>;
     //calculo un numero aleatorio
     var numeroPregunta =  Math.floor(Math.random() * listaPreguntas.length) ;
     //dibujo los textos en los botones correspondientes
-    sigue();
- 
- 
- function sigue(){
-     numeroPregunta =  Math.floor(Math.random() * listaPreguntas.length) ;
+    sigue(); 
+    
+     function sigue(){
+    
+    numeroPregunta =  Math.floor(Math.random() * listaPreguntas.length) ;
     $('#enunciado').text(listaPreguntas[numeroPregunta][1]);
-    $('#r1').text(listaPreguntas[numeroPregunta][2]).click(function(){sigue();});
-    $('#r2').text(listaPreguntas[numeroPregunta][3]).click(function(){sigue();});
-    $('#r3').text(listaPreguntas[numeroPregunta][4]).click(function(){sigue();});
-    $('#r4').text(listaPreguntas[numeroPregunta][5]).click(function(){sigue();});
+    $('#r1').text(listaPreguntas[numeroPregunta][2]);
+    $('#r2').text(listaPreguntas[numeroPregunta][3]);
+    $('#r3').text(listaPreguntas[numeroPregunta][4]);
+    $('#r4').text(listaPreguntas[numeroPregunta][5]);
+    $('#correcta').text(listaPreguntas[numeroPregunta][6]);
  }
  
-    
-</script>    
+ function comprobadora(seguridad){
+     
+     var segur = listaPreguntas[numeroPregunta][6];
+
+        if(segur == seguridad){
+         sigue();
+         
+         tiempo.width(tiempo.width() - caja.width());
+         segundo = "";
+     }
+ } 
+ 
+ 
+</script>
